04015a18d045b9f62bc095ba3b231459e2a36d58
rohithseby <48560429+rohithseby@users.noreply.github.com>
2023-12-12 20:01:50 +0530

    feature: max username 18 chars hokage

--- models/zodschemas.js
+++ models/zodschemas.js
@@ -4,7 +4,7 @@ export const UsernameSchema = z
     .string()
     .trim()
     .min(3, "username should be 3 characters minimum")
-    .max(10, "username should be 10 characters maximum")
+    .max(18, "username should be 18 characters maximum")
     .toLowerCase()
     .refine((s) => !s.includes(" "), "avoid using spaces in your username");
 
